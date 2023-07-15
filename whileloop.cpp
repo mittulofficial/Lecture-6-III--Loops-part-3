803d9b683ff50ef3b03ca94d81a2dd748bdfbe89
mittulofficial <111638490+mittulofficial@users.noreply.github.com>
2023-07-15 22:28:39 +0530

    Create whileloop.cpp
    this is basic in while loop

---
+++ whileloop.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//
+//  Created by MITTUL KUMAR
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  int  i=1;//initiallization
+    while(i<101)
+    {
+        cout<<i<<" ";//loop
+        i++;//update
+    }
+}
