a91965a835b0d4eda4e8a99b5f42a36ffd00f88f
mittulofficial <111638490+mittulofficial@users.noreply.github.com>
2023-07-16 00:45:42 +0530

    Create guessing65.cpp
    this is code about guessing 65 using for loop

---
+++ guessing65.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//
+//  Created by MITTUL KUMAR
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{int input;
+    cout<<"enter no";
+  int  i=100;//initiallization
+    for(i=1;i<101; i++)
+    {
+        cin>>input;
+        if(input==65)
+        {
+            cout<<"congrats you guessed correct";
+            break;
+        }
+       
+    }
+}
